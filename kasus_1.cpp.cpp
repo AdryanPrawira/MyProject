7219acd0a35e81e95fe5fc396e8b5bd559422ef7
AdryanPrawira <61720321+AdryanPrawira@users.noreply.github.com>
2020-06-21 01:02:28 +0800

    Add files via upload

---
+++ kasus_1.cpp.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+#define ll long long
+int gajibersih(float tunjangan, int gajipokok)
+{
+	int gajiBersih = (tunjangan + gajipokok);
+	return gajiBersih;
+}
+
+void hitunggaji(int gajipokok)
+{
+	float tunjangan = 0.15 * gajipokok;
+	float pajak = 0.1 * (tunjangan + gajipokok);
+	cout << "Pajak\t\t: " << fixed << setprecision(0) << "Rp. " << pajak << endl;
+	cout << "Tunjangan\t: " << fixed << setprecision(0) << "Rp. " <<  tunjangan <<endl;
+	cout << "Gaji Bersih\t: " << fixed <<setprecision(0)<< "Rp. "<< gajibersih(tunjangan,gajipokok) - pajak;
+}
+
+void gajitetap(int lembur, int golongan)
+{
+	ll gajitetap;
+	lembur = lembur <= 0 ? 0:lembur;
+	switch(golongan){
+	
+		case 1:
+			gajitetap = 500000 + (lembur * 5000);
+			break;
+		case 2:
+			gajitetap = 300000 + (lembur * 3000);
+			break;
+		case 3:
+			gajitetap = 250000 + (lembur * 2000);
+			break;
+		case 4:
+			gajitetap = 100000 + (lembur * 1500);
+			break;
+		case 5:
+			gajitetap = 50000 + (lembur * 1000);
+			break;
+		default:
+			break;
+	}
+	cout << "\nGaji tetap\t: " <<"Rp. " <<  gajitetap;
+			
+}
+
+int main()
+{
+	
+	int n,totaljamkerja,jml = 0,i,j,simp;	
+	cout << "Jumlah data  : ";
+	cin >> n;
+	string pegawai[n][3], nama;
+	int jam[n][2], gol[n];
+	ll gaji[n];
+	for (i = 0 ; i < n ; i++)
+	{	
+	
+		cout << endl;
+		cout << "Nama: ";
+		cin >>  pegawai[i][0];
+		cout << "NIP: ";
+		cin >>  pegawai[i][1];
+		cout << "Gaji: ";
+		cin >> gaji[i];
+		cout << "Golongan: ";
+		cin >> gol[i];
+		cout << "Jam kerja wajib\t: ";
+		cin >> jam[i][0];
+		cout << "Total jam kerja\t: ";
+		cin >> jam[i][1];
+			
+	}
+	cout << "\nDATA PEGAWAI\n";
+	for( i = 0 ; i < n ; i ++)
+	{
+		cout << endl << i+1 << "--------------------------\n";
+		cout << "Nama: " << pegawai[i][0] << endl; 
+		cout << "NIP: " << pegawai[i][1] << endl;
+		cout << "Gaji: " << gaji[i] << endl; 
+		cout << "Golongan: " << gol[i] << endl;
+		cout << "Total  Jam Kerja: " << jam[i][1] << " jam" <<endl;
+		totaljamkerja = jam[i][1] - jam[i][0];
+		cout << "Jam kerja lembur: " << totaljamkerja<<" jam"<<endl;		
+		hitunggaji(gaji[i]);
+		gajitetap((jam[i][1] - jam[i][0]), gol[i]);
+	}
+		
+	
+	for(i = 0; i<n; i++) {
+   		for(j = i+1; j<n; j++)
+   		{
+      		if(gaji[j] < gaji[i]) {
+        		simp = gaji[i];
+         		gaji[i] = gaji[j];
+        	 	gaji[j] = simp;
+      		}
+   		}
+	}
+
+cout <<"\nUrutan gaji...\n";
+for(i = 0; i<n; i++) {
+   cout <<gaji[i]<<"\n";
+   }
+	cout << "\n\nmencari nama pegawai : ";
+	cin >> nama;
+	
+	for(i = 0 ; i < n ; i ++)
+	{
+		
+		
+		if (nama == pegawai[i][0])
+		{
+			jml++;
+			cout << "Nama: " << pegawai[i][0] << endl; 
+			cout << "NIP: " << pegawai[i][1] << endl;
+			cout << "Gaji: " << gaji[i] << endl; 
+			cout << "Golongan: " << gol[i] << endl;
+			continue;
+		}
+		
+	}
+	cout << "FOUND : " << jml;
+	
+	
+	
+}
+
+
+
---
+++ kasus_2.cpp.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+time_t now = time(0);
+	int count = 1000;
+   	tm *ltm = localtime(&now);
+
+void peminjaman(int menu, string nama, ll id_orang, ll id_pinjam, int day, int month, int year, int jml)
+{
+	
+
+	if (menu == 1)
+	{
+		
+		cout << "PENGEMBALIAN";
+		cout << "Nama: " << nama << "\n";
+		cout << "ID: " << id_orang << "\n";
+		cout << "ID Pinjam: " << id_pinjam << "\n";
+		cout << "Tanggal Peminjaman: " << day << ", " << month << ", " << year << "\n";
+		cout << "Tanggal Pengembalian: " << ltm->tm_mday << ":" << 1 + ltm->tm_mon<< ":" <<  1900 + ltm->tm_year<< "\t\t|\n";
+		if ((day + 7) < (ltm->tm_mday ))
+		{
+			
+			cout << "\nDENDA : Rp. " << (1000 * ((day + 7)   + 6) / 7);
+			
+		}
+		else if(day == ltm->tm_mday ){
+			cout <<"TERIMA KASIH TELAH MENGEMBALIKAN BUKU TEPAT WAKTU\n";
+
+		} else
+		{
+			cout <<"TERIMA KASIH" << endl;
+		}
+	
+		
+	}
+	else if(menu == 2)
+	{
+	
+	
+		cout << "Nama: " << nama << "\n";
+		cout << "Nomor identitas: " << id_orang << "\n";
+		cout << "Nomor ID Pinjam: " << id_pinjam << "\n";
+		cout << "Tanggal Peminjaman : " << ltm->tm_mday << ":" <<  1 + ltm->tm_mon << ":" << 1900 + ltm->tm_year << "\t|\n";
+		cout <<"KEMBALIKAN BUKU SEBELUM TANGGAL: " << day + 7 << ", " << month << ", " << year << endl; 
+	
+	}
+}
+
+
+
+int main()
+{
+		
+	string nama, name;
+	int hh,bb,tttt,pil,jml;
+	ll id_peminjam,id_buku;
+  	
+	cout << "SELAMAT DATANG DI PERPUSTAKAAN BUKU UMARBINKHATTAB\n";
+	cout << "1. PENGEMBALIAN BUKU" << endl;
+	cout << "2. PEMINJAMAN BUKU" <<endl;
+	cout << "3. INFO BUKU HILANG" << endl;
+	cin >> pil;
+	if(pil == 1)
+	{
+		cout << "Nama peminjam\t: \n";
+		cin >> nama;
+		cout << "Nomor ID\t: " << endl;
+		cin >> id_peminjam;
+		cout <<"ID Peminjaman\t: " << endl;
+		cin >> id_buku;
+		cout << "Jumlah buku\t: " << endl;
+		cin >> jml;
+		cout << "Tanggal Peminjaman\t: " << endl;
+		cin >> hh;
+		cout << "Bulan\t\t: " << endl;
+		cin >> bb;
+		cout << "Tahun\t\t: " << endl;
+		cin >> tttt;
+		peminjaman(1,nama,id_peminjam,id_buku,hh,bb,tttt,jml);
+		
+	} else if(pil == 2)
+	{
+		cout << "Nama peminjam\t: " << endl;
+		cin >> nama;
+		cout << "Nomor ID\t: " << endl;
+		cin >> id_peminjam;
+		cout <<"ID Peminjaman\t: " << endl;
+		cin >> id_buku;
+		cout << "Jumlah buku\t: " << endl;
+		cin >> jml;
+		peminjaman(2,nama,id_peminjam, id_buku, bb,hh,tttt,jml);
+		
+		
+	}
+	else if(pil == 3)
+	{
+		cout << "Nama peminjam\t: " << endl;
+		cin >> nama;
+		cout << "Nomor ID\t: " << endl;
+		cin >> id_peminjam;
+		cout <<"ID Peminjaman\t: " << endl;
+		cin >> id_buku;
+		cout << "Jumlah buku\t: " << endl;
+		cin >> jml;
+		cout << "ANDA DIDENDA SEBESAR : Rp. " << jml * (rand() * 1000);
+	}
+	cout << "\n\nSearching nama peminjam : ";
+	cin >> name;
+	cout << "\n-------------------------------------\n";
+	if (name == nama)
+	{
+		cout << "Nama : " << nama << endl;
+		cout << "Nomor ID : " << id_peminjam;
+	} else
+	{
+		cout << "\nTidak ditemukan" << endl;
+	}
+	
+		
+}
+
+
